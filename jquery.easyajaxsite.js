5123c6db905d1d5406da27642a28786742911756
Garpix <info@garpix.com>
2016-02-16 16:16:26 +0300

    init

---
+++ jquery.easyajaxsite.js
@@ -0,0 +1,77 @@
+/**
+ * Garpix Ltd.
+ * (Burenkov Anatoly, Kuznetsov Aleksey)
+ * Site: http://garpix.com
+ * Email: info@garpix.com
+ * Version: 1.0.0
+ * Date: 2016-02-16
+ */
+
+(function($) {
+    $.easyAjaxSite = function(options) {
+        var settings = $.extend( {
+            'updateHtml': ['#content'],
+            'reloadScripts' : [],
+            'onBefore': function(href) { return true; },
+            'onAfter': function(data) { return true; },
+            'onError': function() { alert('Loading page fails.'); return true; },
+            'animation': 'none', // fade, none
+            'animationDuration': 500
+        }, options);
+
+        var animFadeOutIn = function(selector, data) {
+            $(selector).fadeOut(settings.animationDuration, function() {
+                $(selector).html($(data).filter(selector).html());
+                $(selector).fadeIn(settings.animationDuration);
+            });
+        };
+
+        var updateHtmlWithData = function(data, needPushState, url) {
+            for (var i=0, len=settings.updateHtml.length; i < len; i++) {
+                var htmlSelector = settings.updateHtml[i];
+                switch (settings.animation) {
+                    case 'fade':
+                        animFadeOutIn(htmlSelector, data);
+                        break;
+                    default:
+                        $(htmlSelector).html($(data).filter(htmlSelector).html());
+                }
+            }
+            var title = $(data).filter('title').text();
+            if (needPushState) {
+                history.pushState(url, title, url);
+            }
+            for (i=0, len=settings.reloadScripts.length; i < len; i++) {
+                var scriptUrl = settings.reloadScripts[i];
+                $.getScript(scriptUrl);
+            }
+            document.title = title;
+            settings.onAfter(data);
+        };
+
+        var route = function(url, needPushState) {
+            if (settings.onBefore(url)) {
+                $.ajax({
+                    url: url,
+                    dataType: 'html'
+                }).done(function(data) {
+                    updateHtmlWithData(data, needPushState, url);
+                }).fail(function() {
+                    settings.onError();
+                });
+            }
+        };
+
+        $(document).on('click', 'a:not([href^=blob])', function(e) {
+            var url = $(e.target).closest('a').attr('href');
+            route(url, true);
+            return false;
+        });
+
+        window.onpopstate = function(event) {
+            var url = event.state;
+            route(url, false);
+            return false;
+        };
+    };
+})(jQuery);
